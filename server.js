1f88205c43ad5554a4abb21707e6a8e4ef0dc799
PaulValle <paul.valle96@gmail.com>
2019-01-20 23:43:12 -0500

    sentencias

--- server.js
+++ server.js
@@ -33,10 +33,23 @@ app.use(permitirCrossDomain);
 
 
 app.get('/listarUsuarios', (req, res, next) => {
-    
-            return res.json([{"nombre":"Paul Valle"},{"nombre":"Yander Santana"}]);
+    var client = new pg.Client(conString);
+    client.connect(function(err) {
+        if(err) {
+            return console.error('could not connect to postgres', err);
+            return res.status(500).json({success: false, data: err});
+        }
+
+        client.query('SELECT * FROM usuario', function(err, result) {
+            if(err) {
+                return console.error('error running query', err);
+            }
+
+            client.end();
+            return res.json(result.rows);
             
-       
+        }); 
+    });
 });
 
 
