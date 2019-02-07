896712f70df820fb130fed1e4f93ba8d9eef9481
PaulValle <paul.valle96@gmail.com>
2019-02-07 11:08:46 -0500

    jodido mail

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 /* create database JSPROJECT */
 CREATE TABLE usuario (
   idusuario SERIAL PRIMARY KEY,
-  usuario VARCHAR(45) NOT NULL,
+  mail VARCHAR(45) NOT NULL,
   pass VARCHAR(255) NOT NULL,
   nombre VARCHAR(45) NOT NULL,
   rol VARCHAR(45) NOT NULL,
